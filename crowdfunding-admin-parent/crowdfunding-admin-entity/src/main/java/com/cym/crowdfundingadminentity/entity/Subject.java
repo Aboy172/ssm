a22e9a6425c2a067b98d11005c0796ff9a5a802e
cym <cym@gmail.com>
2022-05-24 13:42:34 +0800

    移除jackson依赖

---
+++ crowdfunding-admin-parent/crowdfunding-admin-entity/src/main/java/com/cym/crowdfundingadminentity/entity/Subject.java
@@ -0,0 +1,15 @@
+package com.cym.crowdfundingadminentity.entity;
+
+import lombok.Data;
+
+/**
+ * program: ssm
+ * Date: 2022-03-29  21:40
+ * Author: cym
+ * Description:
+ * @author cym
+ */
+@Data
+public class Subject {
+    private String score;
+}
---
+++ crowdfunding-admin-parent/crowdfunding-admin-entity/src/test/java/com/cym/crowdfundingadminentity/CrowdfundingAdminEntityApplicationTest.java
@@ -0,0 +1,13 @@
+package com.cym.crowdfundingadminentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdfundingAdminEntityApplicationTest {
+
+    @Test
+    void contextLoads ( ) {
+    }
+
+}
